ce4b190cbd18554d0e45a86f2a9922108a21875c
Carlos Aparicio <caparicio@esdmadrid.es>
2022-04-07 10:23:30 +0100

    localstorage

--- app/main.js
+++ app/main.js
@@ -27,34 +27,76 @@ import "phosphor-icons";
 
 
 
+// const get_el_quijote = async () => {
+//     const url = "https://gist.githubusercontent.com/jsdario/6d6c69398cb0c73111e49f1218960f79/raw/8d4fc4548d437e2a7203a5aeeace5477f598827d/el_quijote.txt"
+//     const quijote = await fetch(url).then(s => s.text()).then(d => d)
+//     const words = quijote.split(" ").length
+//     console.log(words)
+// }
+// get_el_quijote()
+
+
+// const get_magikarp = async () => {
+//     const url = "https://pokeapi.co/api/v2/pokemon/magikarp"
+//     const pokemon = await fetch(url).then(s => s.json()).then(d => d)
+//     const pokenname = pokemon.name
+// }
+
+
+
+// /**
+//  * REQUISITO
+//  * 
+//  * Formulario con input y con boton de enviar
+//  * Buscamos pokemon por su nombre
+//  * 
+//  * Si el pokemos existe, pintamos una tarjeta con nombre
+//  * con imagen, y con las habilidades que tiene
+//  * 
+//  * Si el pokemon no existe, pintamos una tarjetita diciendo
+//  * que el pokemon no existe
+//  * Y nos la posibilidad de resetear el formulario
+//  * 
+//  */
+
 
 /**
- * REQUISITO
  * 
- * Formulario con input y con boton de enviar
- * Buscamos pokemon por su nombre
+ * REQUISITO 2
  * 
- * Si el pokemos existe, pintamos una tarjeta con nombre
- * con imagen, y con las habilidades que tiene
+ * Tenemos que hacer persistencia de cada pokemon buscado
+ * Tenemos que renderizar en una lista los pokemon buscados
+ * Recargamos la página, aparecen los pokemon buscados hasta ahora...
+ * Botón que resetee la lista de pokemon buscados
  * 
- * Si el pokemon no existe, pintamos una tarjetita diciendo
- * que el pokemon no existe
- * Y nos la posibilidad de resetear el formulario
+ * REQUISITO 3
+ * Tenemos que guardar el geoposicionamiento de cada búsqueda de pokemon
+ * Mostrarlo en un mapa
  * 
  */
 
 window.addEventListener("load", () => {
+    readLocalStorage()
     initFormEvents()
     initButtonResetEvent()
 })
 
 let searchedPokemon = ""
 let pokemon
+let pokemons = []
 const form = document.querySelector("form")
 const alert = document.querySelector(".alert")
 const button = alert.querySelector("button")
 const pokeHolder = document.querySelector(".pokemon_card_holder")
 const loader = document.querySelector(".loader")
+
+const readLocalStorage = () => {
+    const isPokemonsSet = localStorage.getItem("pokemons") !== null
+    if (isPokemonsSet) {
+        pokemons = JSON.parse(localStorage.getItem("pokemons"))
+    }
+    renderPokemons()
+}
 
 const initFormEvents = () => {
     form.addEventListener("submit", (ev) => {
@@ -72,7 +114,10 @@ const fetchPokemon = async () => {
     pokemon = await fetch(url)
         .then(s => s.json()).then(d => {
             pokemon = d
-            renderPokemon()
+
+            pokemons.push(pokemon)
+            localStorage.setItem("pokemons", JSON.stringify(pokemons))
+            renderPokemons()
             loader.classList.add("hidden")
         })
         .catch(() => {
@@ -82,9 +127,10 @@ const fetchPokemon = async () => {
     
 }
 
-const renderPokemon = () => {
-    console.log(pokemon);
-    pokeHolder.innerHTML = `
+const renderPokemons = () => {
+    let pokemonsHTML = ""
+    pokemons.forEach(pokemon => {
+        pokemonsHTML += `
         <div class="pokemon">
             <h3>${pokemon.name}</h3>
             <picture>
@@ -97,6 +143,8 @@ const renderPokemon = () => {
             </ul>
         </div>
     `
+    })
+    pokeHolder.innerHTML = pokemonsHTML
 }
 
 const renderError = () => {
@@ -111,4 +159,53 @@ const initButtonResetEvent = () => {
     })
 }
 
+
+
+// const now = new Date()
+// const desserts = ["tarta de trufa", "tarta de trufa"]
+// const alumno = {
+//     name: "carlos", 
+//     age: 12323, 
+//     isAlien: true, 
+//     lastVisited: new Date()
+// }
+// const tweets = [
+//     {
+//         tweet: "12323", 
+//         user: {
+//             id: 2, 
+//             name: "carlos"
+//         }, 
+//         likes: 323, 
+//         liked: true, 
+//         comments: 323, 
+//     }, 
+//     {
+//         tweet: "fdgdfgdfgdfgdf", 
+//         user: {
+//             id: 25, 
+//             name: "ana"
+//         }, 
+//         likes: 1323, 
+//         liked: false, 
+//         comments: 1323, 
+//     }
+// ]
+
+
+// localStorage.setItem("my_name", "Carlos")
+// localStorage.setItem("my_age", 323)
+// localStorage.setItem("is_alien", true)
+// localStorage.setItem("last_visited", now.toISOString())
+// localStorage.setItem("my_favorite_desserts", desserts.join(", "))
+// localStorage.setItem("alumno", JSON.stringify(alumno))
+// localStorage.setItem("tweets", JSON.stringify(tweets))
+
+// const name = localStorage.getItem("my_name")
+// const age = +localStorage.getItem("my_age")
+// const isAlien = localStorage.getItem("is_alien") === "true"
+// const lastVisited = new Date(localStorage.getItem("last_visited"))
+// const favDesserts= localStorage.getItem("my_favorite_desserts").split(", ")
+// const alumnoo = JSON.parse(localStorage.getItem("alumno"))
+// const tweets_ = JSON.parse(localStorage.getItem("tweets"))
 
